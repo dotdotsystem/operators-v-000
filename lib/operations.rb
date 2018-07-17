432c60c132b6a400f91bb4e84724f33e462ec3ef
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-17 22:42:47 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,12 @@
 def unsafe?(speed)
   if speed > 60 || speed < 40
-	  return true 
+	  return true
   else
-	  return false 
+	  return false
   end
 end
 
-
-
 def not_safe?(speed)
-
+	if speed > 60 || speed < 40 ? true : false
+  end
 end
